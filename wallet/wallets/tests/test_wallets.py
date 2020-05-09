fb350d875849d8190b4f60191fe4dd72b9da6b0b
Miguel Mesa <mmesas369@gmail.com>
2020-05-09 16:11:39 -0500

    Test for wallets

--- wallet/wallets/tests/test_wallets.py
+++ wallet/wallets/tests/test_wallets.py
@@ -33,5 +33,26 @@ class WalletTests(APITestCase):
         url = "/api/wallets/{}/".format(self.wallet.slug_name)
         response = self.client.get(url)
         serializer = WalletModelSerializer(self.wallet)
-
         self.assertEqual(response.data, serializer.data)
+
+    def test_income(self):
+        """Checks if income process are executed correctly."""
+        url = "/api/wallets/{}/income/".format(self.wallet.slug_name)
+        data = {'amount': 10000}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['balance'], 20000)
+
+    def test_income_none_amount(self):
+        """Checks if BadRequest is returned when no amount is sent."""
+        url = "/api/wallets/{}/income/".format(self.wallet.slug_name)
+        data = {'value': 1000}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_income_negative_value(self):
+        """Returns error if amount is less than one."""
+        url = "/api/wallets/{}/income/".format(self.wallet.slug_name)
+        data = {'amount': 0}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
