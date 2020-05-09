3c49d72f3a13b575f34b35e056221b6294575a86
Miguel Mesa <mmesas369@gmail.com>
2020-05-09 16:11:16 -0500

    Wallet incomes

--- wallet/wallets/serializers.py
+++ wallet/wallets/serializers.py
@@ -2,16 +2,32 @@
 
 # Rest Framework
 
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 # Models
 from .models import Wallet
 
 
-class WalletModelSerializer(ModelSerializer):
+class WalletModelSerializer(serializers.ModelSerializer):
 	"""Wallet model serializer."""
 
 	class Meta:
 		model = Wallet
 		fields = '__all__'
 		read_only_fields = ('balance',)
+
+
+class WalletIncomeSerializer(serializers.ModelSerializer):
+	"""For incomes to wallet."""
+
+	amount = serializers.IntegerField(min_value=1)
+
+	class Meta:
+		model = Wallet
+		fields = ['amount',]
+
+	def update(self, instance, data):
+		wallet = instance
+		wallet.income(data['amount'])
+
+		return wallet
--- wallet/wallets/views.py
+++ wallet/wallets/views.py
@@ -1,18 +1,46 @@
 # Rest Framework
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, viewsets, status
+from rest_framework.decorators import action
+from rest_framework.exceptions import ParseError
+from rest_framework.response import Response
 
 # Models
 from .models import Wallet
 
 # Serializers
 from .serializers import WalletModelSerializer
+from .serializers import WalletIncomeSerializer
 
 
 class WalletViewSet(mixins.RetrieveModelMixin,
 					mixins.ListModelMixin,
+					mixins.UpdateModelMixin,
 					viewsets.GenericViewSet):
 	"""Wallet viewset"""
 
 	serializer_class = WalletModelSerializer
 	queryset = Wallet.objects.all()
 	lookup_field = 'slug_name'
+
+	def get_serializer_class(self):
+		"""Return serializer based on action."""
+		if self.action == 'income':
+			return WalletIncomeSerializer
+
+		return WalletModelSerializer
+
+	@action(detail=True, methods=['post'])
+	def income(self, request, pk=None, *args, **kargs):
+		wallet = self.get_object()
+		serializer_class = self.get_serializer_class()
+		amount = request.data.get('amount')
+		serializer = serializer_class(
+			wallet,
+			data={'amount': amount},
+			partial=True
+		)
+		serializer.is_valid(raise_exception=True)
+		wallet = serializer.save()
+		data = WalletModelSerializer(wallet).data
+		
+		return Response(data, status=status.HTTP_200_OK)
